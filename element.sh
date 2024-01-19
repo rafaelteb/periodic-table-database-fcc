0e08eb8a74d58cb1039a0920b336ad17f86f2126
rafaelteb <rafaelteboekhorst@protonmail.com>
2024-01-19 11:18:30 +0000

    feat: adding first query in case statement

--- element.sh
+++ element.sh
@@ -1,1 +1,18 @@
-echo "Please provide an element as an argument.";
+#!/bin/env bash
+
+# PostgreSQL connection parameters
+DB_NAME="periodic_table"
+DB_USER="freecodecamp"
+
+
+PSQL="psql -d $DB_NAME -U $DB_USER"
+
+case $1 in
+  ''|*[0-9]*)
+    TABLE_OUTPUT=$($PSQL -c "SELECT * FROM properties WHERE atomic_mass = $1");
+		echo $TABLE_OUTPUT;
+    ;;
+	*)
+		echo "Please provide an element as an argument.";
+		;;
+esac
