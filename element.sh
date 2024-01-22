b38992cbb2261ef6ea903d9b2c8295d590b9f859
rafaelteb <rafaelteboekhorst@protonmail.com>
2024-01-22 20:33:45 +0000

    feat: finishing touch to make code more pretty

--- element.sh
+++ element.sh
@@ -1,84 +1,71 @@
 #!/bin/env bash
 shopt -s extglob
+
 # PostgreSQL connection parameters
 DB_NAME="periodic_table"
 DB_USER="freecodecamp"
 PSQL="psql -d $DB_NAME -U $DB_USER"
 
 # Wrong input warning
-EXIT_TEXT="Please provide an element as an argument.";
+EXIT_TEXT="Please provide an element as an argument."
 # Not found text
-NOT_FOUND="I could not find that element in the database.";
+NOT_FOUND="I could not find that element in the database."
+
+# Function to display information about the element
+display_element_info() {
+  ATOMIC_NUMBER=$(echo $1 | cut -d '|' -f 1)
+  TYPE=$(echo $1 | cut -d '|' -f 10)
+  SYMBOL=$(echo $1 | cut -d '|' -f 7)
+  NAME=$(echo $1 | cut -d '|' -f 8)
+  ATOMIC_MASS=$(echo $1 | cut -d '|' -f 2)
+  MELTING_POINT_CELSIUS=$(echo $1 | cut -d '|' -f 3)
+  BOILING_POINT_CELSIUS=$(echo $1 | cut -d '|' -f 4)
+
+  echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
+}
 
 case $1 in
   [0-9]|[0-9][0-9]|[0-9][0-9][0-9])
-		# This block will be executed if $1 is least a one and maximally a 3 digid number
-    TABLE_ELEMENTS_AND_PROPERTIES=$($PSQL -c "SELECT * FROM properties AS p JOIN elements AS e ON p.atomic_number = e.atomic_number JOIN types AS t ON t.type_id = p.type_id WHERE e.atomic_number = '$1';");
-		RESULT=$(echo "$TABLE_ELEMENTS_AND_PROPERTIES" | awk 'NR==3' | tr -d ' ');
+    # Block for numeric input
+    TABLE_ELEMENTS_AND_PROPERTIES=$($PSQL -c "SELECT * FROM properties AS p JOIN elements AS e ON p.atomic_number = e.atomic_number JOIN types AS t ON t.type_id = p.type_id WHERE e.atomic_number = '$1';")
+    RESULT=$(echo "$TABLE_ELEMENTS_AND_PROPERTIES" | awk 'NR==3' | tr -d ' ')
 
-		# If table is empty return NOT_FOUND
-		if [[ "$RESULT" = "(0rows)" ]]
-		then
-			echo $NOT_FOUND;
-		else
-			echo $RESULT;
-			ATOMIC_NUMBER=$(echo $RESULT | cut -d '|' -f 1);
-			TYPE=$(echo $RESULT | cut -d '|' -f 10);
-			SYMBOL=$(echo $RESULT | cut -d '|' -f 7);
-			NAME=$(echo $RESULT | cut -d '|' -f 8);
-			ATOMCIC_MASS=$(echo $RESULT | cut -d '|' -f 2);
-			MELTING_POINT_CELSIUS=$(echo $RESULT | cut -d '|' -f 3);
-			BOILING_POINT_CELSIUS=$(echo $RESULT | cut -d '|' -f 4);
-			echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMCIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
-		fi
-    ;;
-	[A-Za-z]|[A-Za-z][A-Za-z])
-		# This block will be executed if $1 contains at least one and maximally two alphabetic letters
-		TABLE_ELEMENTS_AND_PROPERTIES=$($PSQL -c "SELECT * FROM properties AS p JOIN elements AS e ON p.atomic_number = e.atomic_number JOIN types AS t ON t.type_id = p.type_id WHERE e.symbol = '$1';");
-		RESULT=$(echo "$TABLE_ELEMENTS_AND_PROPERTIES" | awk 'NR==3' | tr -d ' ');
-		
-		# If table is empty return NOT_FOUND
-		if [[ "$RESULT" = "(0rows)" ]]
-		then
-			echo $NOT_FOUND;
-		else
-			ATOMIC_NUMBER=$(echo $RESULT | cut -d '|' -f 1);
-			TYPE=$(echo $RESULT | cut -d '|' -f 10);
-			SYMBOL=$(echo $RESULT | cut -d '|' -f 7);
-			NAME=$(echo $RESULT | cut -d '|' -f 8);
-			ATOMCIC_MASS=$(echo $RESULT | cut -d '|' -f 2);
-			MELTING_POINT_CELSIUS=$(echo $RESULT | cut -d '|' -f 3);
-			BOILING_POINT_CELSIUS=$(echo $RESULT | cut -d '|' -f 4);
-			echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMCIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
-		fi
-		;;
-	*[a-zA-Z]*)
-    # This block will be executed if $1 contains at least one alphabetical character
-    if [[ $1 =~ ^[a-zA-Z]+$ ]]
-		then
-			# This makes sure that $1 is a word without any numbers
-      TABLE_ELEMENTS_AND_PROPERTIES=$($PSQL -c "SELECT * FROM properties AS p JOIN elements AS e ON p.atomic_number = e.atomic_number JOIN types AS t ON t.type_id = p.type_id WHERE e.name = '$1';");
-			RESULT=$(echo "$TABLE_ELEMENTS_AND_PROPERTIES" | awk 'NR==3' | tr -d ' ');
-
-			# If table is empty return NOT_FOUND
-			if [[ "$RESULT" = "(0rows)" ]]
-			then
-				echo $NOT_FOUND;
-			else
-				ATOMIC_NUMBER=$(echo $RESULT | cut -d '|' -f 1);
-				TYPE=$(echo $RESULT | cut -d '|' -f 10);
-				SYMBOL=$(echo $RESULT | cut -d '|' -f 7);
-				NAME=$(echo $RESULT | cut -d '|' -f 8);
-				ATOMCIC_MASS=$(echo $RESULT | cut -d '|' -f 2);
-				MELTING_POINT_CELSIUS=$(echo $RESULT | cut -d '|' -f 3);
-				BOILING_POINT_CELSIUS=$(echo $RESULT | cut -d '|' -f 4);
-				echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMCIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius."
-			fi
+    if [[ "$RESULT" == "(0rows)" ]]; then
+      echo "$NOT_FOUND"
     else
-      echo $EXIT_TEXT;
+      # Display information about the element
+      display_element_info "$RESULT"
     fi
-		;;
-	*)
-		echo $EXIT_TEXT;
-		;;
+    ;;
+  [A-Za-z]|[A-Za-z][A-Za-z])
+    # Block for alphabetic input
+    TABLE_ELEMENTS_AND_PROPERTIES=$($PSQL -c "SELECT * FROM properties AS p JOIN elements AS e ON p.atomic_number = e.atomic_number JOIN types AS t ON t.type_id = p.type_id WHERE e.symbol = '$1';")
+    RESULT=$(echo "$TABLE_ELEMENTS_AND_PROPERTIES" | awk 'NR==3' | tr -d ' ')
+
+    if [[ "$RESULT" == "(0rows)" ]]; then
+      echo "$NOT_FOUND"
+    else
+      # Display information about the element
+      display_element_info "$RESULT"
+    fi
+    ;;
+  *[a-zA-Z]*)
+    # Block for alphabetical input
+    if [[ $1 =~ ^[a-zA-Z]+$ ]]; then
+      TABLE_ELEMENTS_AND_PROPERTIES=$($PSQL -c "SELECT * FROM properties AS p JOIN elements AS e ON p.atomic_number = e.atomic_number JOIN types AS t ON t.type_id = p.type_id WHERE e.name = '$1';")
+      RESULT=$(echo "$TABLE_ELEMENTS_AND_PROPERTIES" | awk 'NR==3' | tr -d ' ')
+
+      if [[ "$RESULT" == "(0rows)" ]]; then
+        echo "$NOT_FOUND"
+      else
+        # Display information about the element
+        display_element_info "$RESULT"
+      fi
+    else
+      echo "$EXIT_TEXT"
+    fi
+    ;;
+  *)
+    echo "$EXIT_TEXT"
+    ;;
 esac
