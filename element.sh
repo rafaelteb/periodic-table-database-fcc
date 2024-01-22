c7ce41b149883edd32e5538501b0db1dcb801bf0
rafaelteb <rafaelteboekhorst@protonmail.com>
2024-01-22 19:46:35 +0000

    fix: adapting output so it is correct

--- element.sh
+++ element.sh
@@ -21,7 +21,6 @@ case $1 in
 		MELTING_POINT_CELSIUS=$(echo $TABLE_PROPERTIES | cut -d '|' -f 9 | tr -d ' ');
 		BOILING_POINT_CELSIUS=$(echo $TABLE_PROPERTIES | cut -d '|' -f 10 | tr -d ' ');
 		echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMCIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius.";
-		echo "ok1";
     ;;
 	[A-Za-z]|[A-Za-z][A-Za-z])
 		# This block will be executed if $1 contains at least one and maximally two alphabetic letters
@@ -35,7 +34,6 @@ case $1 in
 		MELTING_POINT_CELSIUS=$(echo $RESULT | cut -d '|' -f 4);
 		BOILING_POINT_CELSIUS=$(echo $RESULT | cut -d '|' -f 5);
 		echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMCIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_POINT_CELSIUS celsius.";
-		echo "ok2";
 		;;
 	*[a-zA-Z]*)
     # This block will be executed if $1 contains at least one alphabetical character
@@ -45,8 +43,8 @@ case $1 in
 			RESULT=$(echo "$TABLE_ELEMENTS_AND_PROPERTIES" | awk 'NR==3' | tr -d ' ');
 			ATOMIC_NUMBER=$(echo $RESULT | cut -d '|' -f 1);
 			TYPE=$(echo $RESULT | cut -d '|' -f 2);
-			SYMBOL=$1;
-			NAME=$(echo $RESULT | cut -d '|' -f 9);
+			SYMBOL=$(echo $RESULT | cut -d '|' -f 8);
+			NAME=$1
 			ATOMCIC_MASS=$(echo $RESULT | cut -d '|' -f 3);
 			MELTING_POINT_CELSIUS=$(echo $RESULT | cut -d '|' -f 4);
 			BOILING_POINT_CELSIUS=$(echo $RESULT | cut -d '|' -f 5);
